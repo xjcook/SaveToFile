bdfb2e732d9e878f2a04e48046dd12295db8b0d3
XjCook <xjcook@gmail.com>
2016-12-06 22:34:44 +0100

    Clean code

--- app/src/main/java/io/xjhub/savetofile/MainActivity.java
+++ app/src/main/java/io/xjhub/savetofile/MainActivity.java
@@ -7,8 +7,6 @@ import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
     private static final int requestCode = 43;
@@ -18,55 +16,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get intent, action and MIME type
+        // Get intent, action
         Intent intent = getIntent();
         String action = intent.getAction();
-        String type = intent.getType();
 
-        if (Intent.ACTION_SEND.equals(action) && type != null) {
-            if ("text/plain".equals(type)) {
-                handleSendText(intent); // Handle text being sent
-            } else if (type.startsWith("image/")) {
-                handleSendImage(intent); // Handle single image being sent
-            }
-        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
-            if (type.startsWith("image/")) {
-                handleSendMultipleImages(intent); // Handle multiple images being sent
-            }
+        if (Intent.ACTION_SEND.equals(action)) {
+            handleSendFile(intent);
         } else {
-            // Handle other intents, such as being started from the home screen
+            finish();
         }
     }
 
-    protected void handleSendText(Intent intent) {
-        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-        if (sharedText != null) {
-            // Update UI to reflect text being shared
+    protected void handleSendFile(Intent intent) {
+        Uri contentUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (contentUri != null) {
+            Intent docIntent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+            docIntent.addCategory(Intent.CATEGORY_OPENABLE);
+            docIntent.setType(intent.getType());
+            docIntent.putExtra(Intent.EXTRA_TITLE, getFileNameFromUri(contentUri));
+            startActivityForResult(docIntent, requestCode);
         }
     }
 
-    protected void handleSendImage(Intent sentIntent) {
-        Uri imageUri = (Uri) sentIntent.getParcelableExtra(Intent.EXTRA_STREAM);
-        if (imageUri != null) {
-            // Update UI to reflect image being shared
-            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-            intent.setType(sentIntent.getType());
-            intent.putExtra(Intent.EXTRA_TITLE, getFileNameFromUri(imageUri));
-            startActivityForResult(intent, requestCode);
-        }
-    }
-
-    protected void handleSendMultipleImages(Intent intent) {
-        ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-        if (imageUris != null) {
-            // Update UI to reflect multiple images being shared
-        }
-    }
-
-    private String getFileNameFromUri(Uri uri) {
+    private String getFileNameFromUri(Uri contentUri) {
         String fileName = null;
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null, null);
+        Cursor cursor = getContentResolver().query(contentUri, null, null, null, null, null);
 
         try {
             if (cursor != null && cursor.moveToFirst()) {
