e44596016042a922a26df4831d852a2792ad84e2
Ondrej Danko <xjcook@gmail.com>
2016-12-08 13:20:16 +0100

    Add request of permission READ_EXTERNAL_STORAGE

--- app/src/main/java/io/xjhub/savetofile/MainActivity.java
+++ app/src/main/java/io/xjhub/savetofile/MainActivity.java
@@ -1,12 +1,17 @@
 package io.xjhub.savetofile;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.OpenableColumns;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,7 +26,8 @@ import java.io.OutputStream;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "MainActivity";
-    private static final int REQUEST_CODE = 43;
+    private static final int REQUEST_CODE = 41;
+    private static final int REQUEST_READ_EXTERNAL_STORAGE = 51;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +40,46 @@ public class MainActivity extends AppCompatActivity {
         String action = intent.getAction();
 
         if (Intent.ACTION_SEND.equals(action)) {
-            handleSendFile(intent);
+            Uri contentUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+            if (contentUri != null && "file".equals(contentUri.getScheme())) {
+                // Check & request permission when file scheme
+                int permissionCheck = ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_READ_EXTERNAL_STORAGE);
+                } else {
+                    // Permission is already granted
+                    handleSendFile(intent);
+                }
+            } else {
+                // Content scheme does not need to check permission
+                handleSendFile(intent);
+            }
         } else {
             // Finish activity when started manually
             finish();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // We have permission to read file
+                    handleSendFile(getIntent());
+                } else {
+                    // Finish activity when permission was denied
+                    Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                return;
+            }
         }
     }
 
